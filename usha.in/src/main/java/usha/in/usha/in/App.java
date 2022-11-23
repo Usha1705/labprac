ba061eaefa516d404b9634c5ebbdcc85e42039c9
Usha1705 <ushasrivalli2018@gmail.com>
2022-11-23 09:44:39 +0530

    first commit

---
+++ usha.in/src/main/java/usha/in/usha/in/App.java
@@ -0,0 +1,13 @@
+package usha.in.usha.in;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "this is usha" );
+    }
+}
